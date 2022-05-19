9cb30e1d734ed8c81603daa5c567d0dcc0a89375
mkeb1 <kebbellmegan@gmail.com>
2022-05-19 10:11:43 +0200

    hoch und runter tasten entfernt weil die unnötig sind

--- src/Player.java
+++ src/Player.java
@@ -3,9 +3,7 @@ import static java.awt.event.KeyEvent.*;   // das braucht man für die Tasten
 
 public class Player {
     public String colour;    // entweder "red" oder "yellow"
-    public int points;       //
-    public int keyUp;
-    public int keyDown;
+    public int points;
     public int keyLeft;
     public int keyRight;
     public String keys;      // entweder "wasd" oder "arrows"
@@ -14,14 +12,10 @@ public class Player {
         this.colour = colour;
         this.points = points;
         if (Objects.equals(keys, "wasd")) {   // d.h. wenn "wasd" angegeben wird hat der Spieler wasd
-            this.keyUp = VK_W;
-            this.keyDown = VK_S;
             this.keyLeft = VK_A;
             this.keyRight = VK_D;
         }
         if (Objects.equals(keys, "arrows")) {  // d.h. wenn "arrows" angegeben wird hat der Spieler die Pfeiltasten
-            this.keyUp = VK_UP;
-            this.keyDown = VK_DOWN;
             this.keyLeft = VK_LEFT;
             this.keyRight = VK_RIGHT;
         }
