46831bd10bc2a6b1adb18dc85aec5110e96e9c6e
MacBuckPro <tobias-buck@gmx.net>
2022-05-19 10:19:09 +0200

    GUI

--- src/GUI.java
+++ src/GUI.java
@@ -1,7 +1,27 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GUI {
 
+    JFrame jf;
+    Draw draw;
 
+
+    public GUI() {
+        jf = new JFrame("4-tetris");
+        jf.setSize(1920, 1080);
+        jf.getContentPane().setBackground(Color.DARK_GRAY);
+
+        jf.add(piece());
+
+        jf.setVisible(true);
+    }
+
+    public Draw piece() {
+        draw = new Draw();
+        draw.setVisible(true);
+
+        return draw;
+    }
 
 }
