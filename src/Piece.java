a3a12d5171655888c6bded68fbf024369ec9de3c
DerEnthusiast <kalexschlimmer@outlook.de>
2022-05-23 12:39:04 +0200

    Commit Message

--- src/Piece.java
+++ src/Piece.java
@@ -1,2 +1,15 @@
-public class Piece {
+import static java.awt.event.KeyEvent.*;   // das braucht man für die Tasten
+
+public class Piece{
+    public int speed;
+    public String piece_colour;
+
+
+    public Piece(String piece_colour){
+        //piece_colour = Player.getColour();
+    }
+
+    /*public void speed(){
+    }*/
+
 }
