d006b6b8b6a7e6d66be5a563f2472576953c9cc0
Mac Buck Pro <tobias-buck@gmx.net>
2022-05-31 21:12:15 +0200

    das mit den pieces funktioniert jetzt

--- src/DrawCircle.java
+++ src/DrawCircle.java
@@ -8,15 +8,15 @@ import javax.swing.JLabel;
 public class DrawCircle extends JLabel {
 
     Color [] color;
-    int [] x, y, w, h;
-    int nPieces;
+    int [] x, y, w, h ;
+     int nPieces = 0;
 
     public DrawCircle() {
-        int [] x = new int [63];
-        int [] y = new int [63];
-        int [] w = new int [63];
-        int [] h = new int [63];
-        Color [] color = new Color[63];
+        x = new int [63];
+        y = new int [63];
+        w = new int [63];
+        h = new int [63];
+        color = new Color[63];
 
     }
 
@@ -30,16 +30,43 @@ public class DrawCircle extends JLabel {
 
         grafik2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
+        for(int i = 0; i < nPieces; i++) {
+            g.setColor(color[i]);
+            w[i] = 80;
+            h[i] = 80;
+            g.fillOval(x[i], y[i], w[i], h[i]);
+        }
 
-        /*g.setColor(color);
-        g.fillOval(x, y, w, h);*/
 
         repaint();
     }
 
-    public void move(int x, int y) {
+    public void setX(int n, int x) {
+        this.x [n] = x;
+    }
 
+    public void setY(int n, int y) {
+        this.y [n] = y;
+    }
+
+    public void setColor(int n, Color color) {
+        this.color [n] = color;
         repaint();
     }
+
+    public void move(int n, int x, int y) {
+        setX(n, x);
+        setY(n, y);
+        repaint();
+    }
+
+    public int getnPieces() {
+        return nPieces;
+    }
+
+    public void setnPieces(int nPieces) {
+        this.nPieces = nPieces;
+    }
+
 
 }
--- src/GUI.java
+++ src/GUI.java
@@ -10,8 +10,11 @@ public class GUI implements KeyListener {
     DrawCircle drawCircle;
     DrawLine drawLine;
 
+    int zä = 0;
+
     public GUI() {
-        startScreen();
+        //startScreen();
+        startGame();
     }
 
     public void startScreen() {
@@ -24,7 +27,7 @@ public class GUI implements KeyListener {
 
         //klappt noch nicht das muss ich noch verstehen aber hier soll dann eigentlich halt das spiel gestartet werden
         //sobald man die leertaste drückt
-        keyTyped(new KeyEvent(VK_SPACE));
+        //keyTyped(new KeyEvent(VK_SPACE));
     }
 
     public void startGame() {
@@ -32,36 +35,43 @@ public class GUI implements KeyListener {
         jf.setSize(1920, 1080);
         jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         jf.getContentPane().setBackground(Color.DARK_GRAY);
-
-        //addPiece(Color.YELLOW, 500, 500, 50 , 50);
-        //addPiece(Color.GREEN, 600, 600, 50 , 50);
-
-        jf.setVisible(true);
+        jf.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        drawCircle = new DrawCircle();
 
         Grid();
+        jf.setVisible(true);
 
 
-        jf.addKeyListener(new KeyHandler()); //added den Keylistener
+
+        addPiece(Color.YELLOW, 200, 200);
+        addPiece(Color.YELLOW, 400, 400);
+        addPiece(Color.YELLOW, 600, 600);
+
+
+
+        //jf.addKeyListener(new KeyHandler()); //added den Keylistener
 
     }
 
     /**Piece adden und moven **/
 
-    public void movePieceTo(int x, int y) {
-        drawCircle.move(x, y);
+    public void movePieceTo(int n, int x, int y) {
+        drawCircle.move(n, x, y);
         jf.repaint();
    }
 
-   /* public void addPiece(Color color, int x, int y, int w, int h) {
-        jf.add(piece(color, x, y, w, h));
+    public void addPiece(Color color, int x, int y) {
+        zä = zä + 1;
+        drawCircle.setnPieces(zä);
+        drawCircle.setX(drawCircle.getnPieces() - 1, x);
+        drawCircle.setY(drawCircle.getnPieces() - 1, y);
+        drawCircle.setColor(drawCircle.getnPieces() - 1, color);
+        drawCircle.setVisible(true);
+        drawCircle.repaint();
+        jf.add(drawCircle);
         jf.repaint();
     }
 
-    DrawCircle piece(Color color, int x, int y, int w, int h) {
-        //drawCircle = new DrawCircle(color, x, y, w, h);
-        drawCircle.setVisible(true);
-        return drawCircle;
-    }*/
 
     /** Das Spielfeld **/
 
@@ -70,6 +80,7 @@ public class GUI implements KeyListener {
         line1 = new DrawLine(Color.magenta);
         line1.setVisible(true);
         jf.add(line1);
+        jf.repaint();
 
     }
 
